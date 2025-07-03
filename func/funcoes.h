2083b54e45e3047b1fbe4868b6bf360b6c27e13a
aluno <aluno@3E1-E41L.ccet.intra>
2025-07-03 09:16:57 -0300

    alteração na organização dos arquivos

---
+++ func/funcoes.h
@@ -0,0 +1,32 @@
+#ifndef funcoes_h
+#define funcoes_h
+
+#include <string>
+
+struct Cores {
+    int red;
+    int green;
+    int blue;
+};
+
+class Paleta {
+    private:
+        int qnt_cores;
+        Cores cores[100];
+        int valores[100];
+
+    public:
+        Paleta(int qnt = 0, Cores cores[] = {}, int val[] = {});
+        
+        void setColorQuantity(int qnt);
+        void setCores(Cores corlegal[100]);
+        void setValores(int val[100]);
+        
+        int getColorQuantity();
+        Cores* getCores();  // Changed to return pointer
+        int* getValores();   // Changed to return pointer
+        
+        Cores getCorByAltura(int altura);
+};
+
+#endif
---
+++ func/implementacoes.cpp
@@ -0,0 +1,39 @@
+#include "funcoes.h"
+#include <bits/stdc++.h>
+
+Paleta::Paleta(int qnt, Cores cores[], int val[]) {
+    setColorQuantity(qnt);
+    setCores(cores);
+    setValores(val);
+}
+
+void Paleta::setColorQuantity(int qnt) {
+    this->qnt_cores = qnt;
+}
+
+void Paleta::setCores(Cores corlegal[100]) {
+    for (int i = 0; i < qnt_cores; ++i) {
+        this->cores[i] = corlegal[i];
+    }  
+}
+
+void Paleta::setValores(int val[100]) {   
+    for (int i = 0; i < qnt_cores; ++i) {
+        this->valores[i] = val[i];
+    }
+}
+
+int Paleta::getColorQuantity() { return this->qnt_cores; }
+
+Cores* Paleta::getCores() { return this->cores; }  // Changed to return pointer to array
+
+int* Paleta::getValores() { return this->valores; } // Changed to return pointer to array
+
+Cores Paleta::getCorByAltura(int altura) {
+    for (int i = 0; i < this->qnt_cores; i++) {
+        if(altura == valores[i]) {
+            return cores[i];
+        }
+    }
+    return {0, 0, 0};
+}
