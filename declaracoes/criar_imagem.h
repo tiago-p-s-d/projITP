fe8001db5c6b6b4d9dfef307f7a92b476834dfbe
joaoalecsander <joaoalecsandercosta@gmail.com>
2025-07-08 06:06:10 -0300

    Create criar_imagem.h
    Criação do criar_imagem.h

---
+++ declaracoes/criar_imagem.h
@@ -0,0 +1,20 @@
+#ifndef criar_imagem_h
+#define criar_imagem_h
+#include "paleta.h"
+#include <string>
+
+class Imagem {
+    int largura;
+    int altura;
+    Cores** matriz;
+
+public:
+    Imagem(int l, int a);
+    ~Imagem();
+
+    Cores returnPixel(int l, int a);
+    void setPixel(int l, int a, Cores cor);
+    void salvarPPM(const std::string& nome_arquivo, int valor_max);
+};
+
+#endif
