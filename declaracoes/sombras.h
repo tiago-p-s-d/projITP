382507533afb4e4db77e61ac980de8a34d7ffd56
joaoalecsander <joaoalecsandercosta@gmail.com>
2025-07-15 20:18:56 -0300

    Create sombras.h
    Criação do sombras.h

---
+++ declaracoes/sombras.h
@@ -0,0 +1,12 @@
+#ifndef sombras_h
+#define sombras_h
+
+#include "criar_imagem.h"
+#include "criar_mapa_altitude.h"
+
+class Sombras {
+public:
+    void aplicarSombra(Imagem& imagem, MapaAltitudes& mapa);
+};
+
+#endif
